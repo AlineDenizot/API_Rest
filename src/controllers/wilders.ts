d02fdbbe01fe0db7fd6cf03f0f3375125241a76e
Aline Denizot <aline.denizot2@gmail.com>
2021-04-20 11:50:00 +0200

    typescript

--- src/controllers/wilders.ts
+++ src/controllers/wilders.ts
@@ -1,23 +1,23 @@
-const WilderModel = require("../models/Wilder");
+import WilderModel from '../models/Wilder'
+import { Request, Response } from 'express';
 
-module.exports = {
-  create: async (req, res) => {
+export default {
+  create: async (req: Request, res: Response) => {
       await WilderModel.init();
       const wilder = new WilderModel(req.body);
       const result = await wilder.save();
       res.json({success : true, result});
   }, 
-  read: async (req, res) => {
+  read: async (req: Request, res: Response) => {
     const result = await WilderModel.find();
     res.json({success:true, result});
   },
-  update : async (req, res) => {
+  update : async (req: Request, res: Response) => {
     const result = await WilderModel.updateOne({_id: req.body._id}, req.body)
     res.json({success:true, result});
   },
-    delete: async (req, res) => {
+    delete: async (req: Request, res: Response) => {
       const result = await WilderModel.remove({ _id: req.body._id})
       res.json({success:true, result});
-  }
-    
+  }  
 };
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const asyncHandler = require("express-async-handler");
-const cors = require("cors");
+import express from "express";
+import mongoose from "mongoose";
+import asyncHandler from "express-async-handler";
+import cors from "cors";
+import { Request, Response, NextFunction } from 'express';
 
-const wilderController = require("./controllers/wilders");
+import wilderController from "./controllers/wilders";
 
 //Middleware
 const app = express();
@@ -20,7 +21,7 @@ mongoose
     autoIndex: true,
   })
   .then(() => console.log("Connected to database"))
-  .catch((err) => console.log(err));
+  .catch((err: Error) => console.log(err));
 
   //routes
 app.get("/", (req, res) => {
@@ -34,10 +35,14 @@ app.delete("/api/wilder/delete", asyncHandler(wilderController.delete));
 
 app.get("*", (req, res) => {
   res.status(404);
-  res.send({ success: false, message: "Wrong adress" });
+  res.json({ success: false, message: "Wrong adress" });
 });
 
-app.use((error, req, res, next) => {
+interface CustomError extends Error {
+  code: number,
+}
+
+app.use((error: CustomError, req: Request, res: Response, next: NextFunction) => {
   if (error.name === "MongoError" && error.code === 11000) {
     res.status(400);
     res.json({ success: false, message: "The name is already used" });
